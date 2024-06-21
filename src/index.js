0ca70a8ea834c624c78cd9dc3c5c359164e1ccb6
Shubham jaiswal <shubham24cs116@satiengg.in>
2024-06-21 10:45:41 +0530

    Generated Models fror user and vedio also learned about user Schema custom method with JWT and Bcrypt

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import dotenv from  'dotenv'
-import express from 'express';
 import connectDB from './db/index.js';
+import app from "./app.js"
 
 
 dotenv.config({
     path:"./env"
 });
-const app = express()
-connectDB();
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, (req, res) => {
+        console.log(`listening on port${process.env.PORT}`)
+    })
 })
-app.listen(process.env.PORT, (req, res) => {
-    console.log(`listening on port${process.env.PORT}`)
-})
+.catch((error)=>{console.log("Failed to connect", error)});
+
+
+
+
 
